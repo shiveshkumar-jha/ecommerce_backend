d0a9d52ca5c59ffbea9e7aa3dc6f6f20c2edd6ab
shiveshkumar-jha <shiveshkumarjha051@gmail.com>
2026-09-13 01:15:51 +0530

    cart controller is completed

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -24,7 +24,7 @@ const addtocart=asyncHandler(async(req,res)=>{
        })
     }
     else{
-        const existingitems=await cart.items.find(item=>item.product.toString()==productid)
+        const existingitems=await cart.items.find(item=>item.product.toString()===productid)
         if(existingitems){
             const newquantity=existingitems.quantity+quantity
             if(newquantity>product.quantity) throw new apierror(400,"required number of product isn't available")
@@ -49,7 +49,60 @@ const getusercart=asyncHandler(async(req,res)=>{
         select:"name,price,discount,finalprice,image,stock"
     })
     if(!cart) return res.status(200,{},"cart is empty")
-    return res.status(200,cart,"cart fetched successfully")
+    return res.status(200).json(new apiresponse(200,cart,"cart fetched successfully"))
 })
 
-export {addtocart,getusercart}
+const updatequantityincart=asyncHandler(async(req,res)=>{
+    const{productid,quantity}=req.body
+    if(!quantity || quantity<1) throw new apierror(400,"quantity cant be less than 1")
+    if(!productid) throw new apierror(400,"productid is required")
+    const product=await Product.findById(productid)
+    if(!product) throw new apierror(400,"product not found")
+    if(quantity>product.stock) throw new apierror(400,"requred quantity of product isn't available")
+    let cart=await Cart.findOne({user:req.user._id})
+    if(!cart) throw new apierror(400,"cart not found")
+    let item=cart.items.find(item=>item.product.toString()===productid)
+    if(!item) throw new apierror(400,"product not exist in cart")
+    item.quantity=quantity
+    await cart.save({validateBeforeSave:false})
+    return res.status(200).json(new apiresponse(200,cart,"qunatity updated in cart"))
+})
+
+const removefromcart = asyncHandler(async (req, res) => {
+
+    const { productid } = req.params;
+
+    const cart = await Cart.findOne({
+        user: req.user._id
+    });
+
+    if (!cart) {
+        throw new apierror(404, "Cart not found");
+    }
+
+    cart.items = cart.items.filter(
+        item => item.product.toString() !== productid
+    )
+
+    await cart.save({validateBeforeSave:false})
+
+    return res
+        .status(200)
+        .json(
+            new apiresponse(
+                200,
+                cart,
+                "Product removed from cart"
+            )
+        )
+})
+
+const clearcart=asyncHandler(async(req,res)=>{
+    const cart=await Cart.findOne({user:req.user._id})
+    if(!cart) throw new apierror(400,"cart is not found")
+    cart.items=[]
+    await cart.save({validateBeforeSave:false})
+    return res.status(200).json(new apiresponse(200,cart,"cart cleared successfully"))
+})
+
+export {addtocart,getusercart,updatequantityincart,clearcart,removefromcart}
--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -1,12 +1,18 @@
 import { Router } from "express"
 import {verifyjwt} from "../middlewares/auth.middleware.js"
 import { verifyadmin } from "../middlewares/admin.middleware.js"
-import { addtocart,getusercart } from "../controllers/cart.controller.js"
+import { addtocart,getusercart,updatequantityincart ,removefromcart,clearcart} from "../controllers/cart.controller.js"
 
 const router=Router()
 
 router.post("/addtocart",verifyjwt,addtocart)
 
 router.get("/getuserscart",verifyjwt,getusercart)
+
+router.patch("/updateqtyincart".verifyjwt,updatequantityincart)
+
+router.delete("removeitemfromcart",verifyjwt,removefromcart)
+
+router.delete("/clearcart",verifyjwt,clearcart)
 
 export default router
